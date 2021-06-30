33fcb5e81716f2764084c3f671a94fce3b58dd86
Robert Kaussow <mail@thegeeklab.de>
2021-06-30 23:22:03 +0200

    use default render settings

--- src/js/katex-loader.js
+++ src/js/katex-loader.js
@@ -1,11 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
-  renderMathInElement(document.body, {
-    delimiters: [
-      { left: "$$", right: "$$", display: true },
-      { left: "$", right: "$", display: false },
-      { left: "\\(", right: "\\)", display: false },
-      { left: "\\[", right: "\\]", display: true },
-    ],
-    throwOnError: false,
-  });
+  renderMathInElement(document.body);
 });
