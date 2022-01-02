e88ff9fa9b1692a9ff14df026b7731f08593fb20
Robert Kaussow <mail@thegeeklab.de>
2022-01-02 13:34:26 +0100

    revert naming

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const DARK_MODE = "dark"
 export const LIGHT_MODE = "light"
 export const AUTO_MODE = "auto"
-export const THEME = "hugo-geekblog"
+export const THEME = "hugo-geekdoc"
 export const TOGGLE_MODES = [AUTO_MODE, DARK_MODE, LIGHT_MODE]
