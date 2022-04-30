2f5054f38995d4914ff9d44277b9c9823a2c0556
Robert Kaussow <mail@thegeeklab.de>
2022-04-30 13:37:07 +0200

    fix: fix icon on codecopy button (#392)

--- src/js/app.js
+++ src/js/app.js
@@ -14,13 +14,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     if (trigger.hasAttribute("data-copy-feedback")) {
       trigger.classList.add("gdoc-post__codecopy--success")
-      trigger.querySelector(".icon.copy").classList.add("hidden")
-      trigger.querySelector(".icon.check").classList.remove("hidden")
+      trigger.querySelector(".gdoc-icon.copy").classList.add("hidden")
+      trigger.querySelector(".gdoc-icon.check").classList.remove("hidden")
 
       setTimeout(function () {
         trigger.classList.remove("gdoc-post__codecopy--success")
-        trigger.querySelector(".icon.copy").classList.remove("hidden")
-        trigger.querySelector(".icon.check").classList.add("hidden")
+        trigger.querySelector(".gdoc-icon.copy").classList.remove("hidden")
+        trigger.querySelector(".gdoc-icon.check").classList.add("hidden")
       }, 3000)
     }
 
