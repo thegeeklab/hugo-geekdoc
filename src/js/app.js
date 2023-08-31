c4351c7f94ad55d1c36e155dc6a184a9fc822778
Robert Kaussow <mail@thegeeklab.de>
2023-08-31 22:56:40 +0200

    fix: fix hover background color for the code copy box

--- src/js/app.js
+++ src/js/app.js
@@ -8,12 +8,12 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const trigger = e.trigger
 
     if (trigger.hasAttribute("data-copy-feedback")) {
-      trigger.classList.add("gdoc-post__codecopy--success")
+      trigger.classList.add("gdoc-post__codecopy--success", "gdoc-post__codecopy--out")
       trigger.querySelector(".gdoc-icon.copy").classList.add("hidden")
       trigger.querySelector(".gdoc-icon.check").classList.remove("hidden")
 
       setTimeout(function () {
-        trigger.classList.remove("gdoc-post__codecopy--success")
+        trigger.classList.remove("gdoc-post__codecopy--success", "gdoc-post__codecopy--out")
         trigger.querySelector(".gdoc-icon.copy").classList.remove("hidden")
         trigger.querySelector(".gdoc-icon.check").classList.add("hidden")
       }, 3000)
