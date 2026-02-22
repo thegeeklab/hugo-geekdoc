03b0728308190355f455bf8f2c6fa4e87aceb3d5
Robert Kaussow <mail@thegeeklab.de>
2026-02-22 10:54:34 +0100

    modernize js and drop babel

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,5 @@
 import eslint from "@eslint/js"
 import globals from "globals"
-import babelParser from "@babel/eslint-parser"
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 
 export default [
@@ -10,12 +9,34 @@ export default [
       globals: {
         ...globals.browser
       },
-      parser: babelParser,
-      ecmaVersion: 2022,
-      sourceType: "module",
-      parserOptions: {
-        requireConfigFile: false
-      }
+      ecmaVersion: "latest",
+      sourceType: "module"
+    },
+    rules: {
+      "no-var": "error",
+      "prefer-const": "error",
+      "prefer-arrow-callback": "error",
+      "prefer-template": "error",
+      "object-shorthand": "error",
+      "no-loop-func": "error",
+      "no-new-object": "error",
+      "no-array-constructor": "error",
+      "no-prototype-builtins": "error",
+      "prefer-spread": "error",
+      "prefer-rest-params": "error",
+      "default-param-last": "error",
+      "no-useless-constructor": "error",
+      "no-duplicate-imports": "error",
+      "prefer-destructuring": ["error", {
+        "VariableDeclarator": {
+          "array": true,
+          "object": true
+        },
+        "AssignmentExpression": {
+          "array": false,
+          "object": false
+        }
+      }]
     }
   },
   eslintPluginPrettierRecommended
